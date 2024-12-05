5dc7afe1419b47bc2b26132cd1222cbf1fe4a4ff
abby.shinogi <debenporta@gmail.com>
2024-12-04 21:23:41 -0600

    changed landing page to change after click instead of time delay (for demo purposes)

--- quest/lib/pages/landingPage.dart
+++ quest/lib/pages/landingPage.dart
@@ -12,25 +12,23 @@ class LandingPage extends StatefulWidget {
 
 class _SignInState extends State<LandingPage> {
   @override
-  void initState() {
-    super.initState();
-    // Delay the navigation by 2 seconds
-    Future.delayed(const Duration(seconds: 2), () {
-      // Navigate to the next page after 2 seconds
-      Navigator.pushNamed(context, '/onboarding1');
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: OurColors().purpleBackground,
-        body: ListView(children: [
-          const SizedBox(height: 180),
-
-          // SvgPicture.asset("assets/icons/qbLandingLogo.svg",
-          //     height: 200, width: 200),
-          Image.asset('assets/icons/logo.png', height: 220, width: 220),
-        ]));
+      backgroundColor: OurColors().purpleBackground,
+      body: GestureDetector(
+        onTap: () {
+          // Navigate to the next page when the screen is tapped
+          Navigator.pushNamed(context, '/onboarding1');
+        },
+        child: ListView(
+          children: [
+            const SizedBox(height: 180),
+            // SvgPicture.asset("assets/icons/qbLandingLogo.svg",
+            //     height: 200, width: 200),
+            Image.asset('assets/icons/logo.png', height: 220, width: 220),
+          ],
+        ),
+      ),
+    );
   }
 }
