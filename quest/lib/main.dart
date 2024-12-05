f1700bfcd9ec8350a8b7bf97c7682aa7adfa5179
Tyler Saizan <tjsaizan@yahoo.com>
2024-12-04 22:23:16 -0600

    slowed transition to onboarding

--- quest/lib/main.dart
+++ quest/lib/main.dart
@@ -30,9 +30,34 @@ class _MyAppState extends State<MyApp> {
                 color: Colors.black,
                 
         home: const LandingPage(),
+
+        onGenerateRoute: (RouteSettings settings) {
+          switch (settings.name) {
+            case '/onboarding1':
+              return PageRouteBuilder(
+                pageBuilder: (context, animation, secondaryAnimation) {
+                  return const OnboardingPg1(); // The page you want to navigate to
+                },
+                transitionsBuilder: (context, animation, secondaryAnimation, child) {
+                  const begin = Offset(1.0, 0.0);  // Start position (from the right)
+                  const end = Offset.zero;          // End position (current position)
+                  const curve = Curves.easeInOut;
+
+                  var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+                  var offsetAnimation = animation.drive(tween);
+
+                  return SlideTransition(position: offsetAnimation, child: child);
+                },
+                transitionDuration: const Duration(milliseconds: 1000),  // Customize duration
+              );
+            default:
+              return null;
+          }
+        },
+
         routes: {
           '/landingpage': (context) => const LandingPage(),
-          '/onboarding1': (context) => const OnboardingPg1(),
+          // '/onboarding1': (context) => const OnboardingPg1(),
           '/onboarding2': (context) => const OnboardingPg2(),
           '/onboarding3': (context) => const OnboardingPg3(),
           '/signinPage': (context) => const SignInPage(),
